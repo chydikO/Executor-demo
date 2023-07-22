ea7637f6710b478f66534362a8108648e7544469
Chudnovskiy.O.W520 <oleg.chudnovskiy@gmail.com>
2023-07-22 10:16:30 +0300

    00:35

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,15 @@
 package org.example;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         //Example_1_OneThread();
         Example_2_TwoThread();
+        Example_03();
     }
 
     private static void Example_1_OneThread() {
@@ -42,6 +45,20 @@ public class Main {
                 System.out.println(i + " " + Thread.currentThread().getName());
             }
         });
+        executorService.shutdown();
+    }
+
+    private static void Example_03() throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        Future<Long> longFuture = executorService.submit(() -> {
+            var sum = 0L;
+            for (int i = 0; i < 100_000; i++) {
+                sum++;
+            }
+            return sum;
+        });
+        Long result = longFuture.get(); // метод get - блокирующий, аналог join()_
+        System.out.println("result = " + result);
         executorService.shutdown();
     }
 }
